e89f9dc2e95ea226a30398504cbc876d8a0b6e9c
jrpwork <paulojonathan.2k18@gmail.com>
2025-11-12 14:03:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+  // script.js
+  // seleciona o primeiro botão da página
+  const botao = document.querySelector("button");
+  // função que roda quando o botão é clicado
+  botao.addEventListener("click", function() {
+    alert("QUE EU TE AMO MUITO, LUANA SANTOS GOMES MAIA😍😍❤️");
+  }); 
