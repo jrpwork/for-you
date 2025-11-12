733073fbc3ef7a4faa55f16c218e879ec76b5f87
jrpwork <paulojonathan.2k18@gmail.com>
2025-11-12 13:46:33 -0300

    Delete For-you.js

--- For-you.js
+++
@@ -1,7 +0,0 @@
-  // script.js
-  // seleciona o primeiro botão da página
-  const botao = document.querySelector("button");
-  // função que roda quando o botão é clicado
-  botao.addEventListener("click", function() {
-    alert("QUE EU TE AMO MUITO, LUANA SANTOS GOMES MAIS😍😍❤️");
-  }); 
